c51cf6f1bafe2512cd55267ccc0699c73aa13626
ederpbrito <ederpsilva@hotmail.com>
2024-11-11 09:45:41 -0300

    create pipeline to build mongodbatlas and deploy application

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,14 +1,31 @@
+variable "mongodb_username" {
+  description = "MongoDB Atlas username"
+  type        = string
+}
+
+variable "mongodb_password" {
+  description = "MongoDB Atlas password"
+  type        = string
+  sensitive   = true
+}
+
 variable "mongodb_atlas_org_id" {
-  description = "ID da organização no MongoDB Atlas"
+  description = "MongoDB Atlas Organization ID"
   type        = string
 }
 
 variable "mongodb_atlas_public_key" {
-  description = "Chave pública do MongoDB Atlas"
+  description = "MongoDB Atlas Public Key"
   type        = string
 }
 
 variable "mongodb_atlas_private_key" {
-  description = "Chave privada do MongoDB Atlas"
+  description = "MongoDB Atlas Private Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "local_ip" {
+  description = "IP do ambiente de desenvolvimento local"
   type        = string
 }
