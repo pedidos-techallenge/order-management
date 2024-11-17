4c07a8ba0468ea62026c8ba57930594a034ce7ed
ederpbrito <ederpsilva@hotmail.com>
2024-11-17 19:03:42 -0300

    add class

--- src/main/java/br/com/fiap/techchallange/ordermanagement/infrastructure/config/gateways/service/GenerateNumberOrderConfig.java
+++ src/main/java/br/com/fiap/techchallange/ordermanagement/infrastructure/config/gateways/service/GenerateNumberOrderConfig.java
@@ -1,1 +1,15 @@
- 
+package br.com.fiap.techchallange.ordermanagement.infrastructure.config.gateways.service;
+
+import br.com.fiap.techchallange.ordermanagement.adapters.gateways.service.IGenerateNumberOrder;
+import br.com.fiap.techchallange.ordermanagement.infrastructure.gateways.GenerateNumberOrderImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GenerateNumberOrderConfig {
+
+    @Bean
+    public IGenerateNumberOrder generateNumberOrder() {
+        return new GenerateNumberOrderImpl();
+    }
+} 
