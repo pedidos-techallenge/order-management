2f34f4660b071b4c014c11ba30280b930b040981
ederpbrito <ederpsilva@hotmail.com>
2024-12-03 06:24:23 -0300

    ajuste sonarkube

--- src/main/java/br/com/fiap/techchallange/ordermanagement/infrastructure/gateways/DisplayMonitorImpl.java
+++ src/main/java/br/com/fiap/techchallange/ordermanagement/infrastructure/gateways/DisplayMonitorImpl.java
@@ -2,13 +2,16 @@ package br.com.fiap.techchallange.ordermanagement.infrastructure.gateways;
 
 import br.com.fiap.techchallange.ordermanagement.adapters.gateways.service.IDisplayMonitor;
 import br.com.fiap.techchallange.ordermanagement.adapters.presenters.viewmodel.OrderViewModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DisplayMonitorImpl implements IDisplayMonitor {
+    private static final Logger logger = LoggerFactory.getLogger(DisplayMonitorImpl.class);
 
     @Override
     public void display(OrderViewModel orderView) {
-        System.out.println("Display Monitor: " + orderView );
+        logger.info("Display Monitor: {}", orderView);
     }
 }
